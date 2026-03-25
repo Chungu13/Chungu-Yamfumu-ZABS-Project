cd80b59503c0ddc1a6245c4ada0bb5757d5f34d8
Chungu Muloshi <chungumuloshi5086@gmail.com>
2026-03-25 14:22:12 +0200

    Update

--- zabs_project/settings.py
+++ zabs_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(mqqa!)&f#(c90vsr439l0wjqjta_9+%-#7qxx5dc+bs_3v6s1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
